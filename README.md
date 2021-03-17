27ad49867d6dc7ffb41b3c1c98ce4987734ffae1
Caleb Rogers <caleb@calebjay.com>
2021-03-17 15:17:44 +0800

    Dunno if these routes/models/schemas will work but it's the idea

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # crowd-tier-list
 Generate tier lists from a group of people
+
+# Backend
+
+1. Run via
+
+  uvicorn main:app --reload
--- backend/crud.py
+++ backend/crud.py
@@ -28,10 +28,13 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
 
 def create_item(db: Session, item: schemas.ItemCreate):
-    db_item = models.Item(name=item.name, description=item.description )
+    db_item = models.Item(name=item.name, description=item.description)
 
 def get_ratings(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Rating).offset(skip).limit(limit).all()
+
+def get_ratings_by_user_id(db: Session, user_id: int):
+    return db.query(models.Rating).filter(models.Rating.reviewer.owner_id == user_id)
 
 def create_user_rating(db: Session, rating: schemas.RatingCreate, user_id: int, item_id: int, score: int):
     db_rating = models.Rating(**rating.dict(), owner_id=user_id, item_id=item_id, score=score)
